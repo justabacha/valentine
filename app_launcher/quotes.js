4e69c2b4310315b5f7e315721f2fab8b3b34f741
Phestone <just1abacha@gmail.com>
2026-05-07 20:57:43 +0300

    fix

--- app_launcher/quotes.js
+++ app_launcher/quotes.js
@@ -1,5 +1,125 @@
 export const signatureLoops = [
     {
+        part1: "Some people just make the world easier.",
+        part2: "You’re exactly where you need to be.",
+        photo1: "bucket/Image-1.jpg",
+        photo2: "bucket/Image-2.jpg"
+    },
+    {
+        part1: "I don't really do 'ordinary'.",
+        part2: "We’re on a completely different wave.",
+        photo1: "bucket/Image-3.jpg",
+        photo2: "bucket/Image-4.jpg"
+    },
+    {
+        part1: "Everything feels a lot quieter now.",
+        part2: "I finally found a place that feels like home.",
+        photo1: "bucket/Image-5.jpg",
+        photo2: "bucket/Image-6.jpg"
+    },
+    {
+        part1: "My circle is small for a reason.",
+        part2: "You’re the only one who really gets it.",
+        photo1: "bucket/Image-7.jpg",
+        photo2: "bucket/Image-8.jpg"
+    },
+    {
+        part1: "The best things are never planned.",
+        part2: "I’m glad we didn't miss the memo.",
+        photo1: "bucket/Image-9.jpg",
+        photo2: "bucket/Image-10.jpg"
+    },
+    {
+        part1: "Good times + Crazy friends",
+        part2: "Amazing memories!",
+        photo1: "bucket/Image-11.jpg",
+        photo2: "bucket/Image-12.jpg"
+    },
+    {
+        part1: "Side by side or miles apart",
+        part2: "We're always connected at heart.",
+        photo1: "bucket/Image-13.jpg",
+        photo2: "bucket/Image-14.jpg"
+    },
+    {
+        part1: "Laughing through life",
+        part2: "One adventure at a time.",
+        photo1: "bucket/Image-15.jpg",
+        photo2: "bucket/Image-16.jpg"
+    },
+    {
+        part1: "Friendship is treasure",
+        part2: "You are my gold",
+        photo1: "bucket/Image-17.jpg",
+        photo2: "bucket/Image-18.jpg"
+    },
+    {
+        part1: "You're my classic",
+        part2: "In a world full of trends",
+        photo1: "bucket/Image-19.jpg",
+        photo2: "bucket/Image-20.jpg"
+    },
+    {
+        part1: "With a best friend by your side",
+        part2: "Everyday feels like an adventure",
+        photo1: "bucket/Image-21.jpg",
+        photo2: "bucket/Image-22.jpg"
+    },
+    {
+        part1: "You always know how to make me smile",
+        part2: "Even on my worst days",
+        photo1: "bucket/Image-23.jpg",
+        photo2: "bucket/Image-24.jpg"
+    },
+    {
+        part1: "Having you by my side makes me feel brave an' strong",
+        part2: "I cherish all the silly memories we've created together.",
+        photo1: "bucket/Image-25.jpg",
+        photo2: "bucket/Image-26.jpg"
+    },
+    {
+        part1: "Your support gives me wings to fly",
+        part2: "Thanks for believing in me",
+        photo1: "bucket/Image-27.jpg",
+        photo2: "bucket/Image-28.jpg"
+    },
+    {
+        part1: "Our friendship is truly special",
+        part2: "You're the reason I believe in magic :)",
+        photo1: "bucket/Image-29.jpg",
+        photo2: "bucket/Image-30.jpg"
+    },
+    {
+        part1: "Together, we create unforgettable memories",
+        part2: "Friendship is a journey best traveled together.",
+        photo1: "bucket/Image-31.jpg",
+        photo2: "bucket/Image-32.jpg"
+    },
+    {
+        part1: "You bring out the best in me",
+        part2: "Every moment with you is a gift",
+        photo1: "bucket/Image-33.jpg",
+        photo2: "bucket/Image-34.jpg"
+    },
+    {
+        part1: "Laughing with you is my favorite kind of cardio",
+        part2: "In the end we'll all be just a bunch of weirdos laughing at our own inside jokes",
+        photo1: "bucket/Image-35.jpg",
+        photo2: "bucket/Image-36.jpg"
+    },
+    {
+        part2: "You and I are more than friends",
+        part1: "We are like a really small gang of goofballs :)",
+        photo1: "bucket/Image-37.jpg",
+        photo2: "bucket/Image-38.jpg"
+    },
+    {
+        part1: "If you can't laugh at yourself, call me I'll be happy to do it for you",
+        part2: "We're the kind of friends who can turn a boring day into a comedy show",
+        photo1: "bucket/Image-39.jpg",
+        photo2: "bucket/Image-40.jpg"
+    },
+    {
         part1: "Life too short to take it seriously",
         part2: "Thank goodness I've you to laugh with",
         photo1: "bucket/Image-41.jpg",
@@ -16,5 +136,102 @@ export const signatureLoops = [
         part2: "Our friendship is a beautiful journey",
         photo1: "bucket/Image-45.jpg",
         photo2: "bucket/Image-46.jpg"
+    },
+    {
+        part1: "I am lucky to have a Best friend like you",
+        part2: "always better together",
+        photo1: "bucket/Image-47.jpg",
+        photo2: "bucket/Image-48.jpg"
+    },
+    {
+        part1: "I got the priettiest bestie :)",
+        part2: "Life is hard, but at least I have a pretty best friend",
+        photo1: "bucket/Image-49.jpg",
+        photo2: "bucket/Image-50.jpg"
+    },
+    {
+        part1: "Best Friends Forever",
+        part2: "2.2.4 yeah Today, Tomorrow, and Forever",
+        photo1: "bucket/Image-51.jpg",
+        photo2: "bucket/Image-52.jpg"
+    },
+    {
+        part1: "Best friends are hard to find :(",
+        part2: "The best one is already mine :)",
+        photo1: "bucket/Image-53.jpg",
+        photo2: "bucket/Image-54.jpg"
+    },
+    {
+        part1: "We are the perfect match :)",
+        part2: "We go together like copy and paste",
+        photo1: "bucket/Image-55.jpg",
+        photo2: "bucket/Image-56.jpg"
+    },
+    {
+        part1: "I like you because you're wierd like me 😂",
+        part2: "Dear best friend, I Love You 😉",
+        photo1: "bucket/Image-57.jpg",
+        photo2: "bucket/Image-58.jpg"
+    },
+    {
+        part1: "that 'grow together' type friendship",
+        part2: "Some friends are not just friends they are soulmates",
+        photo1: "bucket/Image-59.jpg",
+        photo2: "bucket/Image-60.jpg"
+    },
+    {
+        part1: "I don't know what I did to deserve a best friend like you",
+        part2: "but I'm so grateful to have you in my life",
+        photo1: "bucket/Image-61.jpg",
+        photo2: "bucket/Image-62.jpg"
+    },
+    {
+        part1: "They hate us 'cause they ain't us",
+        part2: "Some connections are priceless ✌️",
+        photo1: "bucket/Image-63.jpg",
+        photo2: "bucket/Image-64.jpg"
+    },
+    {
+        part1: "sometimes u just need to do bad things with good friends",
+        part2: "The best memories comes from the bad ideas done with best friends",
+        photo1: "bucket/Image-65.jpg",
+        photo2: "bucket/Image-66.jpg"
+    },
+    {
+        part1: "But, you're my best friend & I can't lose you",
+        part2: "Thank you for being my unpaid therapist 🙂‍↕️",
+        photo1: "bucket/Image-67.jpg",
+        photo2: "bucket/Image-68.jpg"
+    },
+    {
+        part1: "I love you, idiot.✿..!",
+        part2: "Souls don't meet by accident..❤️!",
+        photo1: "bucket/Image-69.jpg",
+        photo2: "bucket/Image-70.jpg"
+    },
+    {
+        part1: "Cheers to our FRIEND-CHIP!🥂",
+        part2: "u'm.? !t's forever kind thing :)",
+        photo1: "bucket/Image-71.jpg",
+        photo2: "bucket/Image-72.jpg"
+    },
+    {
+        part1: "you SHORT!!, I'm literaly older than you 😂",
+        part2: "Every Tall friend needs a Short best friend🤪",
+        photo1: "bucket/Image-73.jpg",
+        photo2: "bucket/Image-74.jpg"
+    },
+    {
+        part1: "I'll always be there to irritate you",
+        part2: "and till the end you're stuck with me",
+        photo1: "bucket/Image-75.jpg",
+        photo2: "bucket/Image-76.jpg"
+    },
+    {
+        part1: "we'd look cute together.!",
+        part2: "you complete me<3",
+        photo1: "bucket/Image-77.jpg",
+        photo2: "bucket/Image-78.jpg"
     }
+
 ];
